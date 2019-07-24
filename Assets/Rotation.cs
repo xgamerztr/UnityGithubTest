12fa8c459898e39baaa7070ce4c42cccb0398e58
Demirkan <demirkanbuyukoglu@gmail.com>
2019-07-24 16:50:25 +0300

    Rotation Changes

---
+++ Assets/Rotation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+
+    public Vector3 RotationAmountLR;
+
+    public Vector3 RotationAmountUD;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(RotationAmountLR * -1);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(RotationAmountLR);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(RotationAmountUD);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(RotationAmountUD  * -1);
+        }
+    }
+}
