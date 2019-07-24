039e6b0deb13f9ebaa0d350c9891e9a8c8d92ff0
Demirkan <demirkanbuyukoglu@gmail.com>
2019-07-24 17:15:22 +0300

    OnScreen Timer

---
+++ Assets/TimerUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerUp : MonoBehaviour
+{
+
+    public Text startText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startText.text = ("Time Passed:");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startText.text = ("Time Passed: " + Time.fixedTime );
+
+       
+    }
+
+   
+}
