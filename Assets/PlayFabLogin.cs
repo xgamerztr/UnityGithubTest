1323b8285970d61d29d3ff2b877abeb8005eb481
xgamerztr <demirkanbuyukoglu@gmail.com>
2019-07-25 14:35:40 +0200

    Working Login

--- Assets/PlayFabLogin.cs
+++ Assets/PlayFabLogin.cs
@@ -4,23 +4,127 @@ using UnityEngine;
 
 public class PlayFabLogin : MonoBehaviour
 {
+
+    private string userEmail;
+    private string userPassword;
+    private string userName;
+    public GameObject LoginPanel;
+    public GameObject Timer;
+    public GameObject Cube;
+
+    public void GetUserEmail(string emailIn)
+    {
+
+        userEmail = emailIn;
+
+    }
+
+    public void GetUserPassword(string passwordIn)
+    {
+
+        userPassword = passwordIn;
+
+    }
+
+    public void GetUserName(string usernameIn)
+    {
+
+        userName = usernameIn;
+
+    }
+
+    public void OnClickLogIn()
+    {
+
+        var request = new LoginWithEmailAddressRequest
+        {
+
+            Email = userEmail,
+            Password = userPassword
+
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
+
+    }
+
     public void Start()
     {
-        //Note: Setting title Id here can be skipped if you have set the value in Editor Extensions already.
 
-        var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
-        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+        Cube.SetActive(false);
+        Timer.SetActive(false);
+
+        //Note: Setting title Id here can be skipped if you have set the value in Editor Extensions already.
+        if (string.IsNullOrEmpty(PlayFabSettings.TitleId))
+        {
+            PlayFabSettings.TitleId = "F8E62"; // Please change this value to your own titleId from PlayFab Game Manager
+        }
+        //var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
+        //PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+
+        if (PlayerPrefs.HasKey("EMAIL"))
+        {
+
+            userEmail = PlayerPrefs.GetString("EMAIL");
+            userPassword = PlayerPrefs.GetString("PASSWORD");
+
+            var request = new LoginWithEmailAddressRequest
+            {
+
+                Email = userEmail,
+                Password = userPassword
+
+            };
+
+            PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
+        }
+
+        
     }
 
     private void OnLoginSuccess(LoginResult result)
     {
-        Debug.Log("Congratulations, you made your first successful API call!");
+        Debug.Log("Login Succesfull!");
+        PlayerPrefs.SetString("EMAIL", userEmail);
+        PlayerPrefs.SetString("PASSWORD", userPassword);
+        LoginPanel.SetActive(false);
+        Cube.SetActive(true);
+        Timer.SetActive(true);
+    }
+
+    private void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+
+        Debug.Log("Registering Succesfull!");
+        PlayerPrefs.SetString("EMAIL", userEmail);
+        PlayerPrefs.SetString("PASSWORD", userPassword);
+        LoginPanel.SetActive(false);
+        Cube.SetActive(true);
+        Timer.SetActive(true);
+
     }
 
     private void OnLoginFailure(PlayFabError error)
     {
-        Debug.LogWarning("Something went wrong with your first API call.  :(");
-        Debug.LogError("Here's some debug information:");
-        Debug.LogError(error.GenerateErrorReport());
+
+        var registerRequest = new RegisterPlayFabUserRequest
+        {
+
+            Email = userEmail,
+            Password = userPassword,
+            Username = userName
+
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest, OnRegisterSuccess, OnRegisterFailure);
+
     }
+
+    private void OnRegisterFailure(PlayFabError error)
+    {
+
+        Debug.LogError(error.GenerateErrorReport());
+
+    }
+
 }
